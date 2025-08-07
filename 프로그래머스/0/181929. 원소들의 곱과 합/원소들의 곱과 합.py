e28b2ea26264296534294904b7916a837c26f115
berna1925 <forscher2121@gmail.com>
2025-08-07 15:32:37 +0900

    Update 원소들의 곱과 합.py

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,3 +1,6 @@
+# 2025-08-07 15:10 풀이
+0단계는 정답률 50%까지 패스하기로...
+    
 def solution(num_list):
     mul = 1
     sum = 0
@@ -10,3 +13,13 @@ def solution(num_list):
         return 1
     elif mul > sum ** 2:
         return 0
+
+
+**better way to solve**
+
+def solution(num_list):
+    from functools import reduce
+        return int(reduce(lambda x, y: x * y, num_list) < sum(num_list) ** 2)
+
+tip1) map과 유사한 functools.reduce & lambda의 기능을 활용하면 함수적 연산을 쉽게 적용 가능
+tip2) 숫자 (등호/부등호) 숫자 의 결과는 True, False 둘 중 하나이므로 그것을 int로 환산해 결론 내는 방식
