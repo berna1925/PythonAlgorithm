6b81211b6336324ce06707183738ad91f8d36fb9
berna1925 <forscher2121@gmail.com>
2025-08-07 16:24:37 +0900

    Update 유한소수 판별하기.py

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,3 +1,6 @@
+# 2025-08-07 16:20 자력 해결 실패
+최대공약수 함수인 gcd의 존재를 몰랐고 수학적 논리 설계도 실패
+
 def solution(a, b):  
     def gcd(a, b):
         while b:
@@ -11,3 +14,9 @@ def solution(a, b):
             b /= p
     
     return 1 if b == 1 else 2
+
+0단계라는 말이 무색하게 무참하게 패배한 문제.
+
+tip1) 자주 쓰이는 함수인 reduce, gcd, combinations 같은 메서드를 착실히 외우자
+tip2) 어떤 계산 작용을 해야 하는데 그게 뭔지 안 떠오르면 함수 설계를 직접 해보자
+tip3) in iterator 이 꼴은 정말 재앙이다. 눈에 들어올 때까지 사고 연습을 하자
